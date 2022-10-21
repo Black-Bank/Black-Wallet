67a81f7c22cac8dcaf8b9bcc54e3d149f78c2749
Cyro56 <cyrorenato56@gmail.com>
2022-10-20 21:19:45 -0300

    feat: integration with prod

--- BlackCore/src/component/client/provider/clientprovider.ts
+++ BlackCore/src/component/client/provider/clientprovider.ts
@@ -1,6 +1,10 @@
 import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
-
-const link = new HttpLink({uri: 'http://10.0.2.2:4000'});
+const enabledProd = false;
+const link = new HttpLink({
+  uri: enabledProd
+    ? 'https://credit-black.herokuapp.com//'
+    : 'http://10.0.2.2:4000',
+});
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
