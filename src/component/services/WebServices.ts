4e7ff3bbf829a1ab8f6762ee6f6a23888d482adf
Cyro56 <cyrorenato56@gmail.com>
2023-02-11 17:40:16 -0300

    change test

--- src/component/services/WebServices.ts
+++ src/component/services/WebServices.ts
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
 export async function CoinPrice(coin: string) {
-  try {
-    const getPrice_url = `https://api.binance.com/api/v3/avgPrice?symbol=${coin}USDT`;
-    const response = await axios.get(getPrice_url);
-    const coinPrice = Number(response?.data.price);
+  const getPrice_url = `https://api.binance.com/api/v3/avgPrice?symbol=${coin}USDT`;
+  const response = await axios.get(getPrice_url);
+  const coinPrice = Number(response?.data.price);
 
-    return coinPrice;
-  } catch (err: any) {
-    throw new Error(err.message);
-  }
+  return coinPrice;
 }
