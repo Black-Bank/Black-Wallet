f34baa5acdc81cfcd7d138543a4c5a0acb0da79f
Cyro56 <cyrorenato56@gmail.com>
2023-02-11 17:43:39 -0300

    feat/ hack tests

--- src/component/services/WebServices.ts
+++ src/component/services/WebServices.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 export async function CoinPrice(coin: string) {
-  const getPrice_url = `https://api.binance.com/api/v3/avgPrice?symbol=${coin}USDT`;
-  const response = await axios.get(getPrice_url);
-  const coinPrice = Number(response?.data.price);
+  try {
+    const getPrice_url = `https://api.binance.com/api/v3/avgPrice?symbol=${coin}USDT`;
+    const response = await axios.get(getPrice_url);
+    const coinPrice = Number(response?.data.price);
 
-  return coinPrice;
+    return coinPrice;
+  } catch (err: any) {
+    return 0;
+  }
 }
