d5ad77e9573319a7d612a9e0fe915ed0ddccec3c
Igor F. Duca <duccaigor@protonmail.com>
2023-05-02 10:13:09 -0300

    Fix: Testes

--- src/screen/HomeScreen/home.test.tsx
+++ src/screen/HomeScreen/home.test.tsx
@@ -27,7 +27,6 @@ describe('it should render home screen', () => {
       </NavigationContainer>
     );
     render(component);
-    const wallets = await screen.findByText('Carteiras');
     const evoButton = await screen.findByText('Evolução');
     const addButton = await screen.findByText('Adicionar');
     const transferButton = await screen.findByText('Transferir');
@@ -37,6 +36,5 @@ describe('it should render home screen', () => {
     expect(addButton).toBeTruthy();
     expect(transferButton).toBeTruthy();
     expect(trashButton).toBeTruthy();
-    expect(wallets).toBeTruthy();
   });
 });
