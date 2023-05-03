3f992168780a62a5a3676cc01cdbfdb82863bbe8
Makson Sillas <102633441+maksonss4@users.noreply.github.com>
2023-05-03 12:44:18 -0300

    [FIX] svgs da home (#18)
    * [FEAT] svgs da home
    
    * [FIX] correção das svgs da home

---
+++ src/screen/CreateWalletScreen/style.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components/native';
+
+export const ContainerCreateWallet = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  color: #333;
+  margin-bottom: 16px;
+`;
+
+export const InputContainer = styled.View`
+  width: 80%;
+  margin-bottom: 16px;
+`;
+
+export const InputStyled = styled.TextInput`
+  background-color: transparent;
+  height: 40px;
+  border-radius: 8px;
+  border-width: 1px;
+  border-color: #ccc;
+  padding: 8px;
+  color: black;
+`;
+
+export const Error = styled.Text`
+  color: #ffcccc;
+  font-size: 14px;
+  margin-top: 4px;
+`;
+
+export const CreateWalletButton = styled.TouchableOpacity`
+  background-color: #7159c1;
+  justify-content: center;
+  width: 80%;
+  padding: 16px;
+  border-radius: 8px;
+  align-items: center;
+  margin-top: 16px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+`;
