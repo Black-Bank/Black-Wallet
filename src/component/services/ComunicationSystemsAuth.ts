b6538ea41447e72f1dbb16b6336617969461e861
Cyro56 <cyro.costa@ztech.net>
2023-05-21 17:05:25 -0300

    feat: create infra to cripto

--- src/component/services/ComunicationSystemsAuth.ts
+++ src/component/services/ComunicationSystemsAuth.ts
@@ -1,17 +1,22 @@
 import CryptoJS from 'crypto-js';
 import config from '../../../config';
 
+const isProd = Boolean(config.NODE_ENV === 'prod');
 class Crypto {
   public encrypt(plaintext: string): string {
-    const key = CryptoJS.enc.Hex.parse(config.AUTH_PRIVATE_KEY);
-    const iv = CryptoJS.enc.Hex.parse(config.IV);
+    const key = CryptoJS.enc.Hex.parse(
+      isProd ? config.PROD_AUTH_PRIVATE_KEY : config.AUTH_PRIVATE_KEY,
+    );
+    const iv = CryptoJS.enc.Hex.parse(isProd ? config.PROD_IV : config.IV);
     const ciphertext = CryptoJS.AES.encrypt(plaintext, key, {iv}).toString();
     return ciphertext;
   }
 
   public async decrypt(ciphertext: string): Promise<string> {
-    const key = CryptoJS.enc.Hex.parse(config.AUTH_PRIVATE_KEY);
-    const iv = CryptoJS.enc.Hex.parse(config.IV);
+    const key = CryptoJS.enc.Hex.parse(
+      isProd ? config.PROD_AUTH_PRIVATE_KEY : config.AUTH_PRIVATE_KEY,
+    );
+    const iv = CryptoJS.enc.Hex.parse(isProd ? config.PROD_IV : config.IV);
     const bytes = CryptoJS.AES.decrypt(ciphertext, key, {iv});
     const plaintext = bytes.toString(CryptoJS.enc.Utf8);
     return plaintext;
