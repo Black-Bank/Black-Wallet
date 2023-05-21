db0199b76e04063daa8af091adb20aaf3ae8a7e3
Cyro Renato <86036682+Cyro56@users.noreply.github.com>
2023-05-21 17:27:28 -0300

    feat: create infra to cripto (#33)
    Co-authored-by: Cyro56 <cyro.costa@ztech.net>

--- src/component/client/provider/clientprovider.ts
+++ src/component/client/provider/clientprovider.ts
@@ -1,7 +1,8 @@
 import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
 import config from '../../../../config';
 //usar ou nai API de producao
-const enabledProd = false;
+const enabledProd = Boolean(config.NODE_ENV === 'prod');
+
 const link = new HttpLink({
   uri: enabledProd ? config.API_HEROKU : 'http://10.0.2.2:4000',
 });
