e720cb3c24129f7b9a98c107bdf6e43815bc326b
Guilherme Silva <2003silvagui@gmail.com>
2023-05-31 20:02:07 -0300

    fix: corrigindo testes unitarios tela home e wallet list

--- src/screen/WalletListScreen/__tests__/walletList.test.tsx
+++ src/screen/WalletListScreen/__tests__/walletList.test.tsx
@@ -1,24 +1,31 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import {render} from '@testing-library/react-native';
 import {WalletListScreen} from '../../WalletListScreen/WalletListScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {ApolloProvider} from '@apollo/client';
 import {client} from '../../../component/client/provider/clientprovider';
-import {MockedProvider} from '@apollo/client/testing';
 
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
-
-describe('it should render wallet list screen', () => {
-  it('render wallet list screen', async () => {
-    render(
-      <ApolloProvider client={client}>
-        <NavigationContainer>
-          <MockedProvider>
-            <WalletListScreen />
-          </MockedProvider>
-        </NavigationContainer>
-      </ApolloProvider>,
-    );
+const mockedNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useRoute: () => ({
+      name: mockedNavigate,
+    }),
+  };
+});
+describe('WalletListScreen', () => {
+  const component = (
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <WalletListScreen />
+      </NavigationContainer>
+    </ApolloProvider>
+  );
+  it('should render wallet list screen', async () => {
+    render(component);
   });
 });
