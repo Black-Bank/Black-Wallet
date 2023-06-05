c6f583c949b878fef9c3f17ff4f6a3d7183768ad
Guilherme Silva <2003silvagui@gmail.com>
2023-06-05 15:26:06 -0300

    feat: card de excluir e remover wallet

--- src/screen/WalletListScreen/WalletListScreen.tsx
+++ src/screen/WalletListScreen/WalletListScreen.tsx
@@ -3,14 +3,26 @@ import {WalletList} from '../../component/walletList/WalletList';
 import {useGetWallets} from '../../component/hooks/useGetWallets';
 import {AuthContext} from '../../contexts/auth';
 import {Footer} from '../../component/footer/Footer';
-
+import {ScrollView} from 'react-native-gesture-handler';
+import {ViewBanceInfo} from '../../component/cardBalanceInfo/CardBalanceInfo';
+import {Text} from 'react-native-svg';
+import {ContentWalletList} from './WalletList.styles';
+import {CardAddAndDelete} from './CardAddAndDelleteWallet';
 export const WalletListScreen = () => {
   const {data} = useGetWallets();
   const {isUpdate} = useContext(AuthContext);
 
   return (
     <>
-      <WalletList data={data} isUpdate={isUpdate} />
+      <ScrollView>
+        <ContentWalletList>
+          <ViewBanceInfo>
+            <Text>Todas carteiras</Text>
+          </ViewBanceInfo>
+          <CardAddAndDelete />
+          <WalletList data={data} isUpdate={isUpdate} />
+        </ContentWalletList>
+      </ScrollView>
       <Footer />
     </>
   );
