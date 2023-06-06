2a5a37c4be2f1e565e7d6a579ba76886101ec8c2
Guilherme Silva <2003silvagui@gmail.com>
2023-06-06 19:49:49 -0300

    feat: card de porcentagem de carteira

--- src/screen/WalletListScreen/CardsDistribution.styles.js
+++ src/screen/WalletListScreen/CardsDistribution.styles.js
@@ -44,6 +44,100 @@ export const TextNotWallet = styled.Text`
   font-weight: 300;
   font-size: 14px;
   line-height: 20px;
-  width: 118px;
+  width: 90%;
   margin-bottom: 10px;
+`;
+
+export const ContainerGraphic = styled.View`
+  width: 200px;
+  height: 150px;
+  background-color: #ffff;
+  border-radius: 8px;
+`;
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ChartContainer = styled.View`
+  width: 80px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  /* background-color: red; */
+
+  &::after {
+    content: '';
+    position: absolute;
+    background: white;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px; /* Define a borda como metade do tamanho para criar um círculo */
+    z-index: 5;
+  }
+`;
+
+export const OverlayComponent = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -30px; /* Metade da altura */
+  margin-left: -30px; /* Metade da largura */
+  width: 60px;
+  height: 60px;
+  background-color: white;
+  border-radius: 40px;
+  z-index: 3;
+`;
+
+export const TextTitle = styled.Text`
+  font-weight: 300;
+  font-size: 14px;
+  text-align: center;
+  line-height: 20px;
+  margin-top: 7px;
+`;
+
+export const ContentGrapric = styled.View`
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 32px;
+  margin: 0 auto;
+`;
+
+export const ContainerText = styled.View`
+  flex-direction: row;
+`;
+
+export const TextGraphic = styled.Text`
+  margin-left: 5px;
+`;
+
+export const TextPercentageBTC = styled.Text`
+  position: absolute;
+  top: 55%;
+  left: 13%;
+  border-bottom-color: #ff7d00;
+  border-bottom-width: 2;
+`;
+
+export const TextPercentageETH = styled.Text`
+  position: absolute;
+  top: 22%;
+  right: 13%;
+  border-bottom-color: #5b95ff;
+  border-bottom-width: 2;
+`;
+
+export const ContainetNotWallet = styled.TouchableOpacity`
+  width: 200px;
+  height: 150px;
+  background-color: #ffff;
+  border-radius: 8px;
+  justify-content: space-evenly;
+  align-items: center;
 `;
--- src/screen/WalletListScreen/CardsDistribution.tsx
+++ src/screen/WalletListScreen/CardsDistribution.tsx
@@ -2,19 +2,58 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import AddIcon from '../../assets/icon-add.svg';
-import ArrIcon from '../../assets/arr.svg';
+import BTCIcon from '../../assets/icon-btc-wallet.svg';
+import ETHIcon from '../../assets/icon-eth-wallet.svg';
 import {useGetWallets} from '../../component/hooks/useGetWallets';
 import {
   CardCountWallet,
+  ChartContainer,
+  Container,
   ContainerCountAndDistribution,
+  ContainerGraphic,
+  ContainerText,
+  ContainetNotWallet,
+  ContentGrapric,
   ContentTextNotWallet,
   NumberCountWallet,
+  OverlayComponent,
   TextCountWallet,
+  TextGraphic,
   TextNotWallet,
+  TextPercentageBTC,
+  TextPercentageETH,
+  TextTitle,
 } from './CardsDistribution.styles';
+import {PieChart} from 'react-native-chart-kit';
+import {ContentText} from './CardAddAndDelete.styles';
+import {IWallet} from '../HomeScreen/interfaces';
 
 export const CardsDistribuition = () => {
   const {data} = useGetWallets();
+  const countWalletsBTC = data.getFormatedData?.filter(
+    (wallet: IWallet) => wallet.WalletType === 'BTC',
+  ).length;
+  const countWalletsETH = data.getFormatedData?.filter(
+    (wallet: IWallet) => wallet.WalletType === 'ETH',
+  ).length;
+
+  const quantidadeTotal = data.getFormatedData?.length;
+  const porcentagemBTC =
+    quantidadeTotal !== 0
+      ? Math.round((countWalletsBTC / quantidadeTotal) * 100)
+      : 0;
+  const porcentagemETH =
+    quantidadeTotal !== 0
+      ? Math.round((countWalletsETH / quantidadeTotal) * 100)
+      : 0;
+
+  console.log(porcentagemBTC);
+  console.log(porcentagemETH);
+
+  const base = [
+    {name: 'Ethereum', y: porcentagemETH, color: '#5B95FF'},
+    {name: 'Bitcoin', y: porcentagemBTC, color: '#FF7D00'},
+  ];
 
   return (
     <ContainerCountAndDistribution>
@@ -23,7 +62,7 @@ export const CardsDistribuition = () => {
           <TextCountWallet>Você possui</TextCountWallet>
           <NumberCountWallet>{data.getFormatedData.length}</NumberCountWallet>
           <TextCountWallet>
-            {data.getFormatedData.length === 1 ? 'Carteira' : 'Carteiras'}
+            {data.getFormatedData.length > 1 ? 'Carteiras' : 'Carteira'}
           </TextCountWallet>
         </CardCountWallet>
       ) : (
@@ -34,6 +73,56 @@ export const CardsDistribuition = () => {
           <AddIcon width={35} height={35} />
         </ContentTextNotWallet>
       )}
+      {quantidadeTotal !== 0 ? (
+        <ContainerGraphic>
+          <TextTitle>Distribuição da carteira</TextTitle>
+          <PieChartComponent data={base} />
+          <TextPercentageBTC>{porcentagemBTC}%</TextPercentageBTC>
+          <TextPercentageETH>{porcentagemETH}%</TextPercentageETH>
+          <ContentGrapric>
+            <ContainerText>
+              <BTCIcon width={25} height={25} />
+              <TextGraphic>Bitcoin</TextGraphic>
+            </ContainerText>
+
+            <ContainerText>
+              <ETHIcon width={25} height={25} />
+              <TextGraphic>Ethereum</TextGraphic>
+            </ContainerText>
+          </ContentGrapric>
+        </ContainerGraphic>
+      ) : (
+        <ContainetNotWallet>
+          <TextTitle>Distribuição da carteira</TextTitle>
+          <TextNotWallet>
+            Você ainda não possui uma carteira adicionada
+          </TextNotWallet>
+          <AddIcon width={35} height={35} style={{marginBottom: 5}} />
+        </ContainetNotWallet>
+      )}
     </ContainerCountAndDistribution>
+  );
+};
+
+const PieChartComponent = ({data}: any) => {
+  return (
+    <Container>
+      <ChartContainer>
+        <PieChart
+          data={data}
+          width={100}
+          height={100}
+          accessor="y"
+          hasLegend={false}
+          backgroundColor="transparent"
+          paddingLeft="15"
+          chartConfig={{
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          }}
+        />
+        <OverlayComponent />
+      </ChartContainer>
+    </Container>
   );
 };
